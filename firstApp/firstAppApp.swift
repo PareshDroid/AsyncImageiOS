2a52148e7511ce0271901fbc3ba3ff7252ce0e0c
paresh <pareshkulkarni9@gmail.com>
2025-08-05 20:16:10 +0530

    Initial Commit

---
+++ firstApp/firstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstAppApp.swift
+//  firstApp
+//
+//  Created by Paresh on 05/08/25.
+//
+
+import SwiftUI
+
+@main
+struct firstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ firstAppTests/firstAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  firstAppTests.swift
+//  firstAppTests
+//
+//  Created by Paresh on 05/08/25.
+//
+
+import Testing
+@testable import firstApp
+
+struct firstAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
